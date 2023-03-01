bb6512e893dfd2290e22baab445568563ba6d6f9
valyy151 <marin.valenta1@gmail.com>
2023-03-01 13:01:43 +0100

    Initial Commit

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,28 @@
 class MemoryGame {
-  constructor(cards) {
-    this.cards = cards;
-    // add the rest of the class properties here
-  }
+	constructor(cards) {
+		this.cards = cards;
+		this.pickedCards = [];
+		this.pairsClicked = 0;
+		this.pairsGuessed = 0;
+		// add the rest of the class properties here
+	}
 
-  shuffleCards() {
-    // ... write your code here
-  }
+	shuffleCards() {
+		if (!this.cards) {
+			return undefined;
+		} else {
+			for (let i = this.cards.length - 1; i > 0; i--) {
+				const j = Math.floor(Math.random() * (i + 1));
+				[this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+			}
+		}
+	}
 
-  checkIfPair(card1, card2) {
-    // ... write your code here
-  }
+	checkIfPair(card1, card2) {
+		// ... write your code here
+	}
 
-  checkIfFinished() {
-    // ... write your code here
-  }
+	checkIfFinished() {
+		// ... write your code here
+	}
 }
